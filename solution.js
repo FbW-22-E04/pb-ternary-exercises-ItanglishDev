44f2018196ec854325f7ac37d4cfd6b8c12c8b34
chris <chrishethertonweb@gmail.com>
2022-07-11 14:28:39 +0200

    ex 1 to 3 done

---
+++ solution.js
@@ -0,0 +1,16 @@
+const isDog = true;
+isDog === true ? console.log("pat, pat") : console.log("find me a dog to pat!");
+
+//             EXERCISE 2
+
+const speedLimit = 50;
+const yourSpeed = 125;
+
+speedLimit < yourSpeed
+  ? console.log("you're going too fast!")
+  : console.log("You're driving below the speed limit, Oma");
+
+//    EXERCISE 3
+
+const age = 34;
+age < 16 ? console.log("serve butter beer") : console.log("serve beer");
