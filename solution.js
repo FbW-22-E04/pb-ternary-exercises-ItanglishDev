850d4eefc97712a59aa324a7a262fa833131cf1a
chris <chrishethertonweb@gmail.com>
2022-07-11 14:38:27 +0200

    ex 4 to 6 done

--- solution.js
+++ solution.js
@@ -14,3 +14,25 @@ speedLimit < yourSpeed
 
 const age = 34;
 age < 16 ? console.log("serve butter beer") : console.log("serve beer");
+
+const isStudent = false;
+
+isStudent === false
+  ? console.log("Ticket costs €12,00")
+  : console.log("Ticket costs €5,00");
+
+//EXERCISE 4
+const okMarie = "beer";
+okMarie === "cake"
+  ? console.log("Let them eat cake")
+  : console.log("Oh, bother");
+
+//EXERCISE 5
+
+const num1 = 30;
+const num2 = 939;
+const num3 = 40.9;
+
+num1 % 2 === 0 ? console.log(num1, "is even") : console.log(num1, "is odd");
+num2 % 2 === 0 ? console.log(num2, "is even") : console.log(num2, "is odd");
+num3 % 2 === 0 ? console.log(num3, "is even") : console.log(num3, "is odd");
